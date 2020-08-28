d71df12b37fe485a434047f095d6f35443f32b2f
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-08-28 21:33:52 +0530

    Added Carousel

--- src/components/ProjectsSkills.vue
+++ src/components/ProjectsSkills.vue
@@ -1,14 +1,46 @@
 <template>
 <div>
-  <h1>hello0</h1>
+  <h1 class="ml-4 text-2xl font-bold">Projects</h1>
+  <div class="m-auto mt-8 w-3/4">
+    <carousel-3d :controls-visible="true" :clickable="false" :width="600" :height="450" class="carousel">
+      <slide v-for="(slide,i) in slides" v-bind:key="slide.id" :index="i"  class=" bg-white">
+        <div>
+          <img class="mt-2" v-bind:src="slide.img" alt="Synix">
+          <h1 class="text-center mt-4">{{ slide.title }}</h1>
+          <h2 class="text-center mt-4">{{ slide.link }}</h2>
+          <h2></h2>
+        </div>
+      </slide>
+    </carousel-3d>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      slides: [
+        {
+          id: 0,
+          title: 'Synix: A no-flag nodeJs CLI to make the use of command-line easy and uniform',
+          img: './assets/img/Synix.png',
+          link: 'https://github.com/synixjs/synix'
+        },
+        {
+          id: 1,
+          // title: ,
+          // img: ,
+          // link: 
+        }
+      ]
+    };
+  },
 }
 </script>
 
 <style scoped>
+.carousel{
+  background-color: white;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Carousel3d from 'vue-carousel-3d';
 import '@/assets/css/styles.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 Vue.config.productionTip = false;
-
+Vue.use(Carousel3d);
 new Vue({
   router,
   render: h => h(App)
