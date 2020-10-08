921d668e155ebe168daef8b3294a082490887ea5
KunalKatiyar <katiyarkunal2011@gmail.com>
2020-10-08 10:34:13 +0530

    Completed Projects

--- src/components/ProjectsSkills.vue
+++ src/components/ProjectsSkills.vue
@@ -5,9 +5,10 @@
     <carousel-3d :controls-visible="true" :clickable="false" :width="600" :height="450" class="carousel">
       <slide v-for="(slide,i) in slides" v-bind:key="slide.id" :index="i"  class=" bg-white">
         <div>
-          <img class="mt-2" v-bind:src="slide.img" alt="Synix">
-          <h1 class="text-center mt-4">{{ slide.title }}</h1>
-          <h2 class="text-center mt-4">{{ slide.link }}</h2>
+          <h1 class="text-center text-xl font-bold mt-2 mb-4"><span class="rounded-full border p-1 px-3 bg-blue-200">{{ slide.name }}</span></h1>
+          <img class="mt-1" v-bind:src="slide.img" alt="slide.name">
+          <h1 class="text-center mt-4 mb-4"><span class="rounded-lg border p-1 px-2 bg-blue-200">{{ slide.title }}</span></h1>
+          <a v-bind:href="slide.link"><h2 class="text-center mt-1"><span class="border-l-2 border-r-2 border-blue-500 px-1">{{ slide.link }}</span></h2></a>
           <h2></h2>
         </div>
       </slide>
@@ -23,15 +24,31 @@ export default {
       slides: [
         {
           id: 0,
+          name: 'Synix',
           title: 'Synix: A no-flag nodeJs CLI to make the use of command-line easy and uniform',
           img: './assets/img/Synix.png',
           link: 'https://github.com/synixjs/synix'
         },
         {
           id: 1,
-          // title: ,
-          // img: ,
-          // link: 
+          name: 'PPERationalizer',
+          title: 'Backend and Frontend part of PPE-Rationalizer Project. Made in Django and React',
+          img: './assets/img/PPERationalizer.png',
+          link: 'https://github.com/combatcovid19kgp/PPE_rationalizer_backend'
+        },
+        {
+          id: 2,
+          name: 'Chat-Box App',
+          title: 'A ChatBox App for Android built using Kivy',
+          img: './assets/img/ChatBox.png',
+          link: 'https://github.com/KunalKatiyar/ChatBox-App'
+        },
+        {
+          id: 3,
+          name: 'Polling Votes Extractor',
+          title: 'Project to Extract tables from PDFs and compile into CSVs',
+          img: './assets/img/Polling.png',
+          link: 'https://github.com/KunalKatiyar/Polling-Votes-Extractor'
         }
       ]
     };
